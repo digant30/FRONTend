50d406a66465a9bcd6a4da0a25bd49008a412eb7
digant30 <digantgandhi30@gmail.com>
2023-05-01 10:30:00 +0530

    Initial Commit

---
+++ src/pages/Dashboard.js
@@ -0,0 +1,18 @@
+import styles from "../home.module.css";
+
+function App() {
+  return (
+    <div className={styles.main}>
+    <div className={styles.topnav}>
+      <div>
+        <h1> FAKE PRODUCT IDENTIFICATION </h1>
+      </div>
+      <a href="/">Home</a>
+      <a href="login">Manufacturer</a>
+      <a href="sellerlogin">Seller</a>
+    </div>
+    </div>
+  );
+}
+
+export default App;
